32a97109a90a5c88970e2a334e7e06b2dc24f475
Unknown <charlesjgilker@gmail.com>
2018-03-17 12:11:13 -0400

    Auto stash before merge of "chazbranch" and "origin/chazbranch"
    appydoo init commit

--- assets/javascript/appydoo.js
+++ assets/javascript/appydoo.js
@@ -1,1 +1,14 @@
-var win = true;
+
+
+
+// place details
+
+var googP = {
+    api: "https://maps.googleapis.com",
+        path: "/maps/api/place/details/s",
+    output: "json?",
+    placeId: "placeid",
+    apiKey: "AIzaSyCncPas0D_Cgk8gECiV_7tEIqxkNa0f9Ns",
+}
+
+console.log(googP.api + googP.path);
